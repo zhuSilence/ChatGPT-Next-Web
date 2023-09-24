d1dac7e8f7a30844e0bc2ea25db4bac7bd1c2a9e
silence <zhuxiang134@gmail.com>
2023-09-24 11:43:46 +0800

    fix(access): default accesscode

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -205,9 +205,6 @@ export function SideBar(props: { className?: string }) {
             </Link>
           </div>
           <div className={styles["sidebar-action"]}>
-            <a href={HOME_URL} target="_blank" rel="noopener noreferrer">
-              <IconButton icon={<GithubIcon />} shadow />
-            </a>
             <IconButton
               icon={<Lightning />}
               text={
