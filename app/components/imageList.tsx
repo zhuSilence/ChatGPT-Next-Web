add759179b46859e4a98cdb637a841dd71abb480
silence <zhuxiang134@gmail.com>
2023-12-06 20:51:54 +0800

    feat(access): llm 抽象

--- app/components/imageList.tsx
+++ app/components/imageList.tsx
@@ -1,20 +1,20 @@
-import { ImagesResponseDataInner } from "openai";
+import Image from "openai";
 import React, { FC } from "react";
 import styles from "./imageList.module.scss";
 
 interface ImageListProps {
-    images?: ImagesResponseDataInner[];
+  images?: Image[];
 }
 const ImageList: FC<ImageListProps> = ({ images }) => {
-    const singleImage = images && images.length === 1;
+  const singleImage = images && images.length === 1;
 
-    return (
-        <div className={singleImage ? styles.imageGridSingle : styles.imageGrid}>
-            {images &&
-                images.map((image, index) => (
-                    <img key={index} src={image.url} alt={`Image ${index}`} />
-                ))}
-        </div>
-    );
+  return (
+    <div className={singleImage ? styles.imageGridSingle : styles.imageGrid}>
+      {images &&
+        images.map((image, index) => (
+          <img key={index} src={image.url} alt={`Image ${index}`} />
+        ))}
+    </div>
+  );
 };
 export default ImageList;
