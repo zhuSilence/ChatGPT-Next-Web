f7c8ddc40e7ddff641c51cec1c7b4083f453631e
silence <zhuxiang134@gmail.com>
2023-12-30 21:54:36 +0800

    feat(access): llm 抽象

--- app/store/access.ts
+++ app/store/access.ts
@@ -38,7 +38,7 @@ const DEFAULT_ACCESS_STATE = {
 
   // server config
   needCode: true,
-  hideUserApiKey: false,
+  hideUserApiKey: true,
   hideBalanceQuery: false,
   disableGPT4: false,
   disableFastLink: false,
